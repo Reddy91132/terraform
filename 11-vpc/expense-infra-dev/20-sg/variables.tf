d6d5ac930f9efc715743f0931891cfbf06a8805a
yuvasai07 <reddy123hon85@gmail.com>
2024-09-30 15:31:00 +0530

    terraform

---
+++ 11-vpc/expense-infra-dev/20-sg/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+    default = "expense"
+
+}
+
+variable "environment" {
+    default = "dev"
+
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Terraform = "true"
+        environment = "dev"
+    }
+} 
+
+variable "mysql_sg_tags" {
+    default = {
+        component = "mysql"
+    }
+}
